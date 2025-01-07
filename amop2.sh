158a2c34aba4268555210b5efb2eb6085f69064e
amruthesh <amruth1229@gmail.com>
2025-01-07 03:09:00 -0800

    Arthamatic operation Multiplication and Division  2st

---
+++ amop2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Author :- Amruth
+#date :- 28/12/2024
+#dec :- Arithmetic operation
+
+
+read -p " enter the first num :" num1
+read  -p " enter the second num :" num2
+
+multiplication=$((num1 * num2))
+division=$((num1 / num2))
+
+
+echo "The multiplication of $num1 and $num2 is : $multiplication"
+echo "The division of $num1 and $num2 is : $division"
+
+echo "----------Thank You------------"
+
