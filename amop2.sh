566b02798efb1e24ab90361160e9608cb4b9d4de
amruthesh <amruth1229@gmail.com>
2025-01-08 20:57:31 -0800

    Adding addtion operation from the Branch A

--- amop2.sh
+++ amop2.sh
@@ -7,6 +7,7 @@
 read -p " enter the first num :" num1
 read  -p " enter the second num :" num2
 
+
 multiplication=$((num1 * num2))
 division=$((num1 / num2))
 
@@ -16,3 +17,19 @@ echo "The division of $num1 and $num2 is : $division"
 
 echo "----------Thank You------------"
 
+
+
+
+
+
+
+echo " enter the first num"
+read num1
+
+echo " enter the second num"
+read num2
+
+sum=$((num1 + num2))
+
+
+echo "The sum of $num1 and $num2 is : $sum"
